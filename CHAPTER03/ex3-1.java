a0d3692959e2d67d0c2a5023828d96ae130ff7a4
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-01 20:33:56 +0900

    Create ex3-1.java

---
+++ CHAPTER03/ex3-1.java
@@ -0,0 +1,27 @@
+/* 예제 3-1 거스름돈 */
+
+class Main {  
+  public static void main(String args[]) { 
+    int coin[] = {500, 100, 50, 10}; // 동전
+
+    int pay = 121687; // 지출액
+    int charge = 0; // 남은돈
+
+    System.out.println("지출액(원): " + pay);
+    System.out.println();
+
+    for(int i=0; i<4; i++) {
+      if(coin[i] == 500) { // 첫 지출액을 감안해서 가장 큰 돈을 거스름돈으로 환산한다
+        System.out.println(coin[i] + "원 갯수: " + pay / coin[i]);
+        charge = pay - ((pay / coin[i]) * coin[i]);
+        System.out.println("남은돈(원): " + charge);
+        System.out.println();
+      } else { // 나머지 돈들을 거스름돈으로 환산한다
+        System.out.println(coin[i] + "원 갯수: " + charge / coin[i]);
+        charge = charge - ((charge / coin[i]) * coin[i]);
+        System.out.println("남은돈(원): " + charge);
+        System.out.println();
+      }
+    }
+  } 
+}
