aefe036a97e1ee706d1c79d570860fd509d27f5f
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-01 21:20:45 +0900

    Update ex3-1.java

--- CHAPTER03/ex3-1.java
+++ CHAPTER03/ex3-1.java
@@ -4,13 +4,15 @@ class Main {
   public static void main(String args[]) { 
     int coin[] = {500, 100, 50, 10}; // 동전
 
-    int pay = 121687; // 돈
+    int pay = 5680; // 돈
     int coinCnt = 0; // 동전갯수
 
     System.out.println("지출액(원): " + pay);
     System.out.println();
 
-    for(int i=0; i<4; i++) {
+    long start = System.nanoTime();
+
+    for(int i=0; i<coin.length; i++) {
         System.out.println(coin[i] + "원 갯수: " + pay / coin[i]);
         coinCnt += pay / coin[i];
         pay = pay - ((pay / coin[i]) * coin[i]);
@@ -18,6 +20,9 @@ class Main {
         System.out.println();
     }
 
+    long end = System.nanoTime();
+
     System.out.println("동전갯수: " + coinCnt + "개");
-  } 
+    System.out.println("수행시간: " + (end - start) + " ns");
+  }
 }
