ee8b930202ebe66fa6d2ced5b19280b07c91bc9d
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-14 00:22:28 +0900

    Update ex4-4.java

--- CHAPTER04/ex4-4.java
+++ CHAPTER04/ex4-4.java
@@ -2,64 +2,90 @@
 4-4. 게임 개발
 */
 
-//import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-      //Scanner sc = new Scanner(System.in);
+      // 크기 지정
+      int X = 4;
+      int Y = 4;
 
+      // 플레이어의 변위(인덱스 별 - 0: 북, 1: 동, 2: 남, 3: 서)
+      int dx[] = {0, 1, 0, -1};
+      int dy[] = {-1, 0, 1, 0};
+
+      // 맵 시드
       int map[][] = {{1, 1, 1, 1},
                      {1, 0, 0, 1},
                      {1, 1, 0, 1},
-                     {1, 1, 1, 1}}; // 맵 시드
+                     {1, 1, 1, 1}}; 
 
-      int C[] = {1, 1}; // 캐릭터의 좌표(x, y)
-      int cnt = 0; // 방문한 칸의 수
+      // 캐릭터의 좌표 지정(y, x)
+      int c[] = {2, 2};
 
-      boolean out = true;
+      // 방문한 칸의 수
+      int cnt = 0;
 
-      while(out) {
-        System.out.println();
-        if((map[C[1]][C[0]] != 1) && (C[1] >= 0)) { // 북
-          map[C[1]][C[0]] = 2;
-          C[1]--;
-          cnt++;
-          System.out.println("방문한 칸의 수: " + cnt);
-        } else if((map[C[1]][C[0]] != 1) && (C[0] <= 3)) { // 동
-          map[C[1]][C[0]] = 2;
-          C[0]++;
-          cnt++;
-          System.out.println("방문한 칸의 수: " + cnt);
-        } else if((map[C[1]][C[0]] != 1) && (C[1] <= 3)) { // 남
-          map[C[1]][C[0]] = 2;
-          C[1]++;
-          cnt++;
-          System.out.println("방문한 칸의 수: " + cnt);
-        } else if((map[C[1]][C[0]] != 1) && (C[0] >= 0)) { // 서
-          map[C[1]][C[0]] = 2;
-          C[0]--;
-          cnt++;
-          System.out.println("방문한 칸의 수: " + cnt);
-        }
-        System.out.println();
-
-        for(int i=0; i<4; i++) {
-          for(int j=0; j<4; j++) {
-
-
-            if(map[i][j] == 1) {
-              System.out.println("[x]: " + j + " [y]: " + i +  " => " + map[i][j]);
-            } else if(map[i][j] == 2) {
-              System.out.println("[x]: " + j + " [y]: " + i +  " => " + map[i][j]);
-            } else {
-              System.out.println("[x]: " + j + " [y]: " + i +  " => " + map[i][j]);
-              out = false;
-            }
-          }
-          System.out.println("==================");
-        }
+      outerOut:
+      while(true) {
+        System.out.println("현재 지형: " + map[c[1]][c[0]]);
+        System.out.println("현재 좌표(y, x): " + c[1] + ", " + c[0]);
+        innerOut:
+        switch(1) {
+          case 1:
+              if(c[0] >= 0) { // 북
+                if(map[c[1]+dy[0]][c[0]+dx[0]] == 0) {
+                  map[c[1]][c[0]] = 2;
+                  c[0] += dy[0];
+                  c[1] += dx[0];
+                  cnt++;
+                  System.out.println("이동한 지형: " + map[c[1]][c[0]]);
+                  System.out.println("이동한 좌표(y, x): " + c[1] + ", " + c[0]);
+                  System.out.println("방문한 칸의 수: " + cnt);
+                  System.out.println();
+                  break innerOut;
+                }
+              } else if(c[1] <= (X-1)) { // 동
+                if(map[c[1]+dy[1]][c[0]+dx[1]] == 0) {
+                  map[c[1]][c[0]] = 2;
+                  c[0] += dy[1];
+                  c[1] += dx[1];
+                  cnt++;
+                  System.out.println("이동한 지형: " + map[c[1]][c[0]]);
+                  System.out.println("이동한 좌표(y, x): " + c[1] + ", " + c[0]);
+                  System.out.println("방문한 칸의 수: " + cnt);
+                  System.out.println();
+                  break innerOut;
+                }
+              } else if(c[0] <= (Y-1)) { // 남
+                if(map[c[1]+dy[2]][c[0]+dx[2]] == 0) {
+                  map[c[1]][c[0]] = 2;
+                  c[0] += dy[2];
+                  c[1] += dx[2];
+                  cnt++;
+                  System.out.println("이동한 지형: " + map[c[1]][c[0]]);
+                  System.out.println("이동한 좌표(y, x): " + c[1] + ", " + c[0]);
+                  System.out.println("방문한 칸의 수: " + cnt);
+                  System.out.println();
+                  break innerOut;
+                }               
+              } else if(c[1] >= 0) { // 서
+                if(map[c[1]+dy[3]][c[0]+dx[3]] == 0) {
+                  map[c[1]][c[0]] = 2;
+                  c[0] += dy[3];
+                  c[1] += dx[3];
+                  cnt++;
+                  System.out.println("이동한 지형: " + map[c[1]][c[0]]);
+                  System.out.println("이동한 좌표(y, x): " + c[1] + ", " + c[0]);
+                  System.out.println("방문한 칸의 수: " + cnt);
+                  System.out.println();
+                  break innerOut;
+                }
+              }
+            
+          default:
+            break outerOut; // 갈 곳 없으면 루프 탈출
+        }    
       }
 
-      //sc.close();
+      System.out.println("최종적으로 이동한 칸의 수: " + cnt);
     }
 }
