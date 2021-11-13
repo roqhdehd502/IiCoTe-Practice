fe085ca8782b57cf122f5efa7d01c99620aedec9
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-13 23:07:10 +0900

    Update ex4-3.java

--- CHAPTER04/ex4-3.java
+++ CHAPTER04/ex4-3.java
@@ -15,88 +15,74 @@ ex-2) a1에 있을 때 이동할 수 있는 경우의 수는 2가지이다.
 : 2칸, 4칸, 6칸, 8칸
 */
 
-import java.util.Scanner;
-
 class Main {
     public static void main(String[] args) {
-      Scanner sc = new Scanner(System.in);
+      // 크기 지정
+      int X = 8;
+      int Y = 8; 
 
-      // 체스판의 좌표
-      //String chess[][] = {{"a1","b1","c1","d1","e1","f1","g1","h1"},
-      //                   {"a2","b2","c2","d2","e2","f2","g2","h2"},
-      //                   {"a3","b3","c3","d3","e3","f3","g3","h3"},
-      //                   {"a4","b4","c4","d4","e4","f4","g4","h4"},
-      //                   {"a5","b5","c5","d5","e5","f5","g5","h5"},
-      //                   {"a6","b6","c6","d6","e6","f6","g6","h6"},
-      //                   {"a7","b7","c7","d7","e7","f7","g7","h7"},
-      //                   {"a8","b8","c8","d8","e8","f8","g8","h8"}};
+      // 나이트의 좌표 지정(    )
+      int kn[] = {1, 1}; // y, x
+
+      // 나이트의 변위
+      int dx[] = {2, 1, -2, -1};
+      int dy[] = {2, 1, -2, -1};
+
+      // 경우의 수 카운팅
+      int cnt = 0;
       
-      // 나이트의 좌표 지정 
-      System.out.print("나이트의 좌표 입력(ex. a1): ");
-      String knight = sc.nextLine();
-
-      if(knight.equals("a1") || knight.equals("a8") || knight.equals("h8") || knight.equals("h1")) {
-        System.out.println("2");
-
-      } else if(knight.equals("c1") || knight.equals("d1") || knight.equals("e1") || knight.equals("f1")
-        || knight.equals("a3") || knight.equals("a4") || knight.equals("a5") || knight.equals("a6")
-        || knight.equals("c8") || knight.equals("d8") || knight.equals("e8") || knight.equals("f8")
-        || knight.equals("h3") || knight.equals("h4") || knight.equals("h5") || knight.equals("h6")) {
-        System.out.println("4");
-
-      } else if(knight.equals("c2") || knight.equals("c3") || knight.equals("c4") || knight.equals("c5") || knight.equals("c6") || knight.equals("c7")
-        || knight.equals("b6") || knight.equals("d6") || knight.equals("e6") || knight.equals("f6") || knight.equals("g6")
-        || knight.equals("f2") || knight.equals("f3") || knight.equals("f4") || knight.equals("f5") || knight.equals("f7")
-        || knight.equals("b3") || knight.equals("d3") || knight.equals("e3") || knight.equals("g3")) {
-        System.out.println("6");
-
-      } else {
-        System.out.println("8");
+      // 나이트가 이동할 수 있는 경로대로 카운트하기
+      switch(1) {
+        case 1: // 1시방향
+          if((kn[0]+dy[1] <= Y) && (kn[1]+dx[0] <= X)) { cnt++; }
+          System.out.println(kn[0]+dy[1]);
+          System.out.println(kn[1]+dx[0]);
+          System.out.println(cnt);
+          System.out.println();
+        case 2: // 2시방향
+          if((kn[0]+dy[0] <= Y) && (kn[1]+dx[1] <= X)) { cnt++; }
+          System.out.println(kn[0]-dy[0]);
+          System.out.println(kn[1]-dx[1]);
+          System.out.println(cnt);
+          System.out.println();
+        case 4: // 4시방향
+          if((kn[0]+dy[3] >= 1) && (kn[1]+dx[0] <= X)) { cnt++; }
+          System.out.println(kn[0]-dy[3]);
+          System.out.println(kn[1]-dx[0]);
+          System.out.println(cnt);
+          System.out.println();
+        case 5: // 5시방향
+          if((kn[0]+dy[2] >= 1) && (kn[1]+dx[1] <= X)) { cnt++; }
+          System.out.println(kn[0]-dy[2]);
+          System.out.println(kn[1]-dx[1]);
+          System.out.println(cnt);
+          System.out.println();
+        case 7: // 7시방향
+          if((kn[0]+dy[2] >= 1) && (kn[1]+dx[3] >= 1)) { cnt++; }
+          System.out.println(kn[0]-dy[2]);
+          System.out.println(kn[1]-dx[3]);
+          System.out.println(cnt);
+          System.out.println();
+        case 8: // 8시방향
+          if((kn[0]+dy[3] >= 1) && (kn[1]+dx[2] >= 1)) { cnt++; }
+          System.out.println(kn[0]-dy[3]);
+          System.out.println(kn[1]-dx[2]);
+          System.out.println(cnt);
+          System.out.println();
+        case 10: // 10시방향
+          if((kn[0]+dy[1] <= Y) && (kn[1]+dx[2] >= 1)) { cnt++; }
+          System.out.println(kn[0]-dy[1]);
+          System.out.println(kn[1]-dx[2]);
+          System.out.println(cnt);
+          System.out.println();
+        case 11: // 11시방향
+          if((kn[0]+dy[0] <= Y) && (kn[1]+dx[3] >= 1)) { cnt++; }
+          System.out.println(kn[0]-dy[0]);
+          System.out.println(kn[1]-dx[3]);
+          System.out.println(cnt);
+          System.out.println();
       }
 
-      sc.close();
-        
-        
-        ==========================================
-		// 초기 좌표 지정
-		int kn[] = {0, 2}; // y, x
-
-		// 변위
-		int dx[] = {2, 1, -2, -1};
-		int dy[] = {2, 1, -2, -1};
-
-		// 경우의 수 카운팅
-		int cnt = 0;
-
-		switch() {
-			case 1:
-			case 2:
-			case 4:
-			case 5:
-			case 7:
-			case 8:
-			case 10:
-			case 11:
-		}
-
-		// 나이트가 이동 가능한 경로
-		if(chess[kn[0]-dy[1]][kn[1]-dx[0]]) { // 1시방향
-		
-		} else if(chess[kn[0]-dy[0]][kn[1]-dx[1]]) { // 2시방향
-
-		} else if(chess[kn[0]-dy[3]][kn[1]-dx[0]]) { // 4시방향
-
-		} else if(chess[kn[0]-dy[2]][kn[1]-dx[1]]) { // 5시방향
-
-		} else if(chess[kn[0]-dy[2]][kn[1]-dx[3]]) { // 7시방향
-
-		} else if(chess[kn[0]-dy[3]][kn[1]-dx[2]]) { // 8시방향
-		
-		} else if(chess[kn[0]-dy[1]][kn[1]-dx[2]]) { // 10시방향
-
-		} else if(chess[kn[0]-dy[0]][kn[1]-dx[3]]) { // 11시방향
-
-		}
-        ==========================================
+      System.out.println(cnt);
     }
 }
