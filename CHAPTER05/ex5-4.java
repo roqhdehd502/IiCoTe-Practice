39b627858b9fff26e1882f0019f552dba964511f
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-15 20:07:45 +0900

    Update ex5-4.java

--- CHAPTER05/ex5-4.java
+++ CHAPTER05/ex5-4.java
@@ -1,13 +1,9 @@
 /* 
 5-4. 미로 탈출: 문제 설명
 
-- 동빈이는 N × M 크기의 직사각형 형태의 미로에 갇혔다. 미로에는 여러 마리의 괴물이 있어 이를
-피해 탈출해야 한다
-- 동빈이의 위치는 (1, 1)이며 미로의 출구는 (N, M)의 위치에 존재하며 한 번에 한 칸씩 이동할 수 있다.
-이때 괴물이 있는 부분은 0으로, 괴물이 없는 부분은 1로 표시되어 있다. 미로는 반드시 탈출할 수 있는
-형태로 제시된다
-- 이때 동빈이가 탈출하기 위해 움직여야 하는 최소 칸의 개수를 구하라. 칸을 셀 때는 시작 칸과 마지막 칸을
-모두 포함해서 계산한다
+- 동빈이는 N × M 크기의 직사각형 형태의 미로에 갇혔다. 미로에는 여러 마리의 괴물이 있어 이를 피해 탈출해야 한다
+- 동빈이의 위치는 (1, 1)이며 미로의 출구는 (N, M)의 위치에 존재하며 한 번에 한 칸씩 이동할 수 있다. 이때 괴물이 있는 부분은 0으로, 괴물이 없는 부분은 1로 표시되어 있다. 미로는 반드시 탈출할 수 있는 형태로 제시된다
+- 이때 동빈이가 탈출하기 위해 움직여야 하는 최소 칸의 개수를 구하라. 칸을 셀 때는 시작 칸과 마지막 칸을 모두 포함해서 계산한다
 
 입력 조건
 ========
@@ -31,13 +27,12 @@
 10
 */
 
-//import java.io.*;
 import java.util.*;
 
 public class Main {
   // 크기 설정
   public static int n, m;
-  public static int[][] graph = new int[1000][1000];
+  public static int[][] maze = new int[(n*m)][(n*m)];
 
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
@@ -51,7 +46,7 @@ public class Main {
     for (int i = 0; i<n; i++) {
       String str = sc.nextLine();
       for (int j = 0; j < m; j++) {
-        graph[i][j] = str.charAt(j) - '0';
+        maze[i][j] = str.charAt(j) - '0';
       }
     }
 
@@ -65,26 +60,33 @@ public class Main {
       System.out.println("현재 위치: " + i + ", " + j);
 
       // 최대한 우측 하단으로 이동
-      if (graph[i+1][j] == 1) { 
-        graph[i][j] = 2;
+      if (maze[i+1][j] == 1) { 
+        maze[i][j] = 2;
         i++;
         result++;
         System.out.println("변위: " + i + ", " + j);
         System.out.println("result: " + result);
         System.out.println();
-      } else if (graph[i][j+1] == 1) {
-        graph[i][j] = 2;
+      } else if (maze[i][j+1] == 1) {
+        maze[i][j] = 2;
         j++;
         result++;
         System.out.println("변위: " + i + ", " + j);
         System.out.println("result: " + result);
         System.out.println();
-      } else if(graph[i-1][j] == 2) {
+      } else if (maze[i][j-1] == 1) {
+        maze[i][j] = 2;
+        j++;
+        result++;
+        System.out.println("변위: " + i + ", " + j);
+        System.out.println("result: " + result);
+        System.out.println();  
+      } else if(maze[i-1][j] == 2) {
         i--;
         System.out.println("변위: " + i + ", " + j);
         System.out.println("result: " + result);
         System.out.println();
-      } else if(graph[i][j-1] == 2) {
+      } else if(maze[i][j-1] == 2) {
         j--;
         System.out.println("변위: " + i + ", " + j);
         System.out.println("result: " + result);
