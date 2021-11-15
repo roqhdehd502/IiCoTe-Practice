9af4a7e51197d223605f3026fa6c522606d79e22
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-15 11:31:35 +0900

    Update ex5-3.java

--- CHAPTER05/ex5-3.java
+++ CHAPTER05/ex5-3.java
@@ -2,73 +2,61 @@
 5-3. 음료수 얼려먹기
 */
 
+import java.io.*;
 import java.util.*;
 
-class Main {
-    public static void main(String[] args) {
-      //Scanner sc = new Scanner(System.in);
-      // Stack<Integer> stack = new Stack<>(); //int형 스택 선언
+public class Main {
 
-      int N = 4; // 세로길이
-      int M = 5; // 가로길이
+    public static int n, m;
+    public static int[][] graph = new int[1000][1000];
 
-      int arr[][] = {{0, 0, 1, 1, 0},
-                     {0, 0, 0, 1, 1},
-                     {1, 1, 1, 1, 1},
-                     {0, 0, 0, 0, 0}};
-
-      int cntY[] = new int[M];
-      int cntX[] = new int[N];
-      int isCon = 0; // 연속 혹은 불연속인 라인이 있는지 카운트
-
-      for(int i=0; i<N; i++) {
-        for(int j=0; j<M; j++) {
-          System.out.print(arr[i][j] + " ");
+    // DFS로 특정 노드를 방문하고 연결된 모든 노드들도 방문
+    public static boolean dfs(int x, int y) {
+        // 주어진 범위를 벗어나는 경우에는 즉시 종료
+        if (x <= -1 || x >=n || y <= -1 || y >= m) {
+            return false;
         }
-        System.out.println();
-      }
-      System.out.println();       
-
-      for(int i=0; i<N; i++) { // X축에서 연속적인 라인이 있는지 확인
-        for(int j=1; j<M; j++) {
-          if( (arr[i][j] == 0) && (arr[i][j] == arr[i][j-1]) ) {
-            isCon = 1;
-          }
+        // 현재 노드를 아직 방문하지 않았다면
+        if (graph[x][y] == 0) {
+            // 해당 노드 방문 처리
+            graph[x][y] = 1;
+            // 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
+            dfs(x - 1, y);
+            dfs(x, y - 1);
+            dfs(x + 1, y);
+            dfs(x, y + 1);
+            return true;
         }
-        cntX[i] = (isCon >= 1) ? isCon : 0;
-        isCon = 0;
-      }
-      for(int i=0; i<M; i++) { // Y축에서 연속적인 라인이 있는지 확인
-        for(int j=1; j<N; j++) {
-          if( (arr[j][i] == 0) && (arr[j][i] == arr[j-1][i]) ) {
-            isCon = 1;
-          }
-        }
-        cntY[i] = (isCon >= 1) ? isCon : 0;
-        isCon = 0;
-      }
-
-      System.out.println("X축에서 연속하는지의 여부");
-      for(int i=0; i<cntX.length; i++) {
-        System.out.print(cntX[i] + " ");
-      }
-      System.out.println();
-      System.out.println("Y축에서 연속하는지의 여부");
-      for(int i=0; i<cntY.length; i++) {
-        System.out.print(cntY[i] + " ");
-      }
-      System.out.println();
-
-      for(int i=0; i<cntX.length; i++) {
-        for(int j=0; j<cntY.length; j++) {
-          cntX[i]^cntY[j]
-        }
-      }
-      
-
-      
-      
-
-      //sc.close();
+        return false;
     }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // N, M을 공백을 기준으로 구분하여 입력 받기
+        n = sc.nextInt();
+        m = sc.nextInt();
+        sc.nextLine(); // 버퍼 지우기
+
+        // 2차원 리스트의 맵 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            String str = sc.nextLine();
+            for (int j = 0; j < m; j++) {
+                graph[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+        // 모든 노드(위치)에 대하여 음료수 채우기
+        int result = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                // 현재 위치에서 DFS 수행
+                if (dfs(i, j)) {
+                    result += 1;
+                }
+            }
+        }
+        System.out.println(result); // 정답 출력 
+    }
+
 }
