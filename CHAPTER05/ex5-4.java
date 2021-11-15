07bd8b8392bc94117b2b52aa599f77454e59241c
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-15 15:08:28 +0900

    Update ex5-4.java

--- CHAPTER05/ex5-4.java
+++ CHAPTER05/ex5-4.java
@@ -39,34 +39,15 @@ public class Main {
   public static int n, m;
   public static int[][] graph = new int[1000][1000];
 
-  public static boolean bfs(int x, int y) {
-    // 주어진 범위를 벗어나는 경우에는 즉시 종료
-    if (x <= -1 || x >=n || y <= -1 || y >= m) {
-      return false;
-    }
-    // 현재 노드를 아직 방문하지 않았다면
-    if (graph[x][y] == 1) {
-      // 해당 노드 방문 처리
-      graph[x][y] = 0;
-      // 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
-      dfs(x - 1, y);
-      dfs(x, y - 1);
-      dfs(x + 1, y);
-      dfs(x, y + 1);
-      return true;
-    }
-    return false;
-  }
-
   public static boolean dfs(int x, int y) {
     // 주어진 범위를 벗어나는 경우에는 즉시 종료
     if (x <= -1 || x >=n || y <= -1 || y >= m) {
       return false;
     }
-    // 현재 노드를 아직 방문하지 않았다면
-    if (graph[x][y] == 0) {
+    // 노드에 괴물이 없으면
+    if (graph[x][y] == 1) {
       // 해당 노드 방문 처리
-      graph[x][y] = 1;
+      //graph[x][y] = 0;
       // 상, 하, 좌, 우의 위치들도 모두 재귀적으로 호출
       dfs(x - 1, y);
       dfs(x, y - 1);
