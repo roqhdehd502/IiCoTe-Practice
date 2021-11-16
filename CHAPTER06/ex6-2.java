343fe5370a15d2a977cf1fca0b97099e24c0b68f
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-16 21:17:49 +0900

    Update ex6-2.java

--- CHAPTER06/ex6-2.java
+++ CHAPTER06/ex6-2.java
@@ -8,7 +8,8 @@ import java.util.Scanner;
 class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
-    int n = 4;
+
+    int n = Integer.parseInt(sc.nextLine());
     int arr[] = new int[n];
 
     for(int i=0; i<arr.length; i++) {
