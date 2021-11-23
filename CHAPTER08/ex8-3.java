e45c128f9651c0d1798b2721e62a09b3fab7e700
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-11-23 20:49:23 +0900

    Update ex8-3.java

--- CHAPTER08/ex8-3.java
+++ CHAPTER08/ex8-3.java
@@ -19,15 +19,24 @@ class Main {
 
     int n = sc.nextInt();
     int[] k = new int[n];
-    int m1 = 0;
-    int m2 = 0;
-    
     for(int i=0; i<k.length; i++) { k[i] = sc.nextInt(); }
     sc.close();
 
-    for(int i=0; i<k.length; i+=2) { m1 += k[i]; }
-    for(int i=1; i<k.length; i+=2) { m2 += k[i]; }
+    // int m1 = 0;
+    // int m2 = 0;
+    // for(int i=0; i<k.length; i+=2) { m1 += k[i]; }
+    // for(int i=1; i<k.length; i+=2) { m2 += k[i]; }
 
-    System.out.println(Math.max(m1, m2));
+    // System.out.println(Math.max(m1, m2));
+    
+    int[] d = new int[100];
+
+    d[0] = k[0];
+    d[1] = Math.max(k[0], k[1]);
+    for(int i=2; i<n; i++) {
+      d[i] = Math.max(d[i-1], d[i-2]+k[i]);
+    }
+
+    System.out.println(d[n-1]);
   }
 }
