71dbc7861f843441bbc48c4aae45b841dd952cac
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-12-29 17:30:20 +0900

    Update ex11-5.java

--- CHAPTER11/ex11-5.java
+++ CHAPTER11/ex11-5.java
@@ -21,9 +21,21 @@ class Main {
     int m = sc.nextInt(); // 공의 최대 무게(1 <= m <= 10)
 
     int[] k = new int[n]; // 각 볼링공의 무게(1 <= k <= m)
-    for(int i=0; i<b.length; i++) { k[i] = sc.nextInt(); }
+    for(int i=0; i<k.length; i++) { k[i] = sc.nextInt(); }
     sc.close();
 
-  
+    int answer = n*(n-1)/2; // 중복되지 않는 볼링공을 고를때의 모든 경우의 수
+    int temp = 0;
+
+    for(int i=0; i<k.length; i++) {
+      for(int j=temp; j<k.length; j++) {
+        if((k[i] == k[j])) {
+          answer--; // 무게가 서로 다른 볼링공을 골라야한다
+          temp++;
+        }
+      }
+    }
+
+    System.out.println(answer);
   }
 }
