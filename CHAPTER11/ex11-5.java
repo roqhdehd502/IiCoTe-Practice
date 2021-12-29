ff1828ff04c02c87ac165ca2223272757bab53f1
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2021-12-29 17:19:40 +0900

    Create ex11-5.java

---
+++ CHAPTER11/ex11-5.java
@@ -0,0 +1,29 @@
+/**
+11-5. 볼링공 고르기
+
+입력예시
+=====
+5 3
+1 3 2 3 2
+
+출력예시
+=====
+8
+*/
+
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int n = sc.nextInt(); // 볼링공의 개수(1 <= n <= 1000)
+    int m = sc.nextInt(); // 공의 최대 무게(1 <= m <= 10)
+
+    int[] k = new int[n]; // 각 볼링공의 무게(1 <= k <= m)
+    for(int i=0; i<b.length; i++) { k[i] = sc.nextInt(); }
+    sc.close();
+
+  
+  }
+}
