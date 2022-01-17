320f4f813564c551980bf7f7538e4f0db9372083
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-01-17 19:03:39 +0900

    Update ex11-6.java

--- CHAPTER11/ex11-6.java
+++ CHAPTER11/ex11-6.java
@@ -1,5 +1,12 @@
 /**
 11-6. 무지의 먹방 라이브
+
+=== 입력 예시 ===
+food_times => [3, 1, 2]
+k => 5
+
+=== 출력 예시 ===
+result => 1
 */
 
 import java.util.Scanner;
@@ -8,31 +15,35 @@ class Main {
   public static void main(String[] args) {
     Scanner sc = new Scanner(System.in);
 
+    int cnt = 0; // 시간 카운트
     int n = sc.nextInt(); // 음식의 번호 지정
-    int k = sc.nextInt(); // 네트워크 장애가 발생한 시간
-    int cost = 0; // 무지가 모든 음식을 먹는 시간
     int[] food_times = new int[n]; // 각 음식을 모두 먹는 데 필요한 시간이 담겨 있는 배열
 
-    boolean isReset = false; // 무지가 음식을 다 먹지 못했을 경우를 체크하는 변수
+    // 각 음식을 모두 먹는데 걸리는 시간 설정
+    for(int i=0; i<food_times.length; i++) { food_times[i] = sc.nextInt(); }
+
+    int k = sc.nextInt(); // 네트워크 장애가 발생한 시간
+    sc.close();
 
     for(int i=0; i<food_times.length; i++) {
+      if(cnt == k) { // 장애가 발생한 시간 
+        System.out.println(k + "초에서 네트워크 장애가 발생했습니다."
+          + (i+1) + "번 음식을 섭취해야 할 때 중단되었으므로, 장애 복구 후에 "
+          + (i+1) + "번 음식부터 다시 먹기 시작하면 됩니다.");
+        break;  
+      }
+
       if(food_times[i] <= 0) {
-        food_times[i] = -1; // 더 섭취해야 할 음식이 없다면 -1을 반환
+        continue;
       } else {
         food_times[i]--; // 무지가 음식 하나를 섭취하는 시간
-        cost++;
+        System.out.println(cnt + " ~ " + (cnt+1) + "초 동안 "
+          + (i+1) + "번 음식을 섭취한다. 해당 음식 번호의 남은 시간은 "
+          + food_times[i] + "입니다.");
+        cnt++;
       }
 
-      for(int j=0; j<food_times.length; j++) {
-        if(food_times))
-        food_times[]
-      }
+      if(i == food_times.length-1) { i = 0; }
     }
-
-    sc.close();
   }
-
-  //private static int solution() {
-  //  
-  //}
 }
