1fd7525b46f2587680cbaaae9246b03e56b26d48
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-01-17 19:38:47 +0900

    Update ex11-6.java

--- CHAPTER11/ex11-6.java
+++ CHAPTER11/ex11-6.java
@@ -31,25 +31,19 @@ class Main {
     int k = sc.nextInt(); // 네트워크 장애가 발생한 시간
     sc.close();
 
-    for(int i=0; i<food_times.length; i++) {
-      if(cnt == k) { // 장애가 발생한 시간 
+    for(int i=0; i<k; i++) {
+      if(i == k-1) {
         System.out.println(k + "초에서 네트워크 장애가 발생했습니다."
-          + (i+1) + "번 음식을 섭취해야 할 때 중단되었으므로, 장애 복구 후에 "
-          + (i+1) + "번 음식부터 다시 먹기 시작하면 됩니다.");
-        break;  
+          + (food_times[i%food_times.length]+1) + "번 음식을 섭취해야 할 때 중단되었으므로, 장애 복구 후에 "
+          + (food_times[i%food_times.length]+1) + "번 음식부터 다시 먹기 시작하면 됩니다.");
       }
 
-      if(food_times[i] <= 0) {
-        continue;
-      } else {
-        food_times[i]--; // 무지가 음식 하나를 섭취하는 시간
-        System.out.println(cnt + " ~ " + (cnt+1) + "초 동안 "
-          + (i+1) + "번 음식을 섭취한다. 해당 음식 번호의 남은 시간은 "
-          + food_times[i] + "입니다.");
-        cnt++;
+      if([i%food_times.length] > 0) {
+        food_times[i%food_times.length]--;
+        System.out.println(i + " ~ " + (i+1) + "초 동안 "
+          + (food_times[i%food_times.length]+1) + "번 음식을 섭취한다. 해당 음식 번호의 남은 시간은 "
+          + food_times[i%food_times.length] + "입니다.");
       }
-
-      if(i == food_times.length) { i = 0; }
     }
   }
 }
