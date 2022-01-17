3b12bbc1d9a6f22d3052941201dd8cd3abae387c
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-01-17 19:51:39 +0900

    Update ex11-6.java

--- CHAPTER11/ex11-6.java
+++ CHAPTER11/ex11-6.java
@@ -27,22 +27,22 @@ class Main {
     // 각 음식을 모두 먹는데 걸리는 시간 설정
     for(int i=0; i<food_times.length; i++) { food_times[i] = sc.nextInt(); }
 
-    int k = sc.nextInt(); // 네트워크 장애가 발생한 시간
+    int k = sc.nextInt(); // 네트워크 장애가 발생하는 시간 지점
     sc.close();
 
-    for(int i=0; i<k; i++) {
-      if(i == k-1) {
-        System.out.println(k + "초에서 네트워크 장애가 발생했습니다. "
-          + (food_times[i%food_times.length]+1) + "번 음식을 섭취해야 할 때 중단되었으므로, 장애 복구 후에 "
-          + (food_times[i%food_times.length]+1) + "번 음식부터 다시 먹기 시작하면 됩니다.");
-      }
-
+    int temp = 0;
+    for(int i=0; i<=k; i++) {
       if(food_times[i%food_times.length] > 0) {
-        food_times[i%food_times.length]--;
+        temp = i%food_times.length;
+        food_times[temp]--;
         System.out.println(i + " ~ " + (i+1) + "초 동안 "
-          + (food_times[i%food_times.length]+1) + "번 음식을 섭취한다. 해당 음식 번호의 남은 시간은 "
-          + food_times[i%food_times.length] + "입니다.");
+          + (temp+1) + "번 음식을 섭취한다. 해당 음식 번호의 남은 시간은 "
+          + food_times[temp] + "입니다.");
       }
     }
+
+    System.out.println(k + "초에서 네트워크 장애가 발생했습니다. "
+          + (temp+1) + "번 음식을 섭취해야 할 때 중단되었으므로, 장애 복구 후에 "
+          + (temp+1) + "번 음식부터 다시 먹기 시작하면 됩니다.");
   }
 }
