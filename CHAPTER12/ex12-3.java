e8098199157a5bcdeb6156b314ee2a44583760f8
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-01-18 18:58:41 +0900

    Create ex12-3.java

---
+++ CHAPTER12/ex12-3.java
@@ -0,0 +1,23 @@
+/**
+12-3. 문자열 압축
+=== 입력 예시.1 ===
+aabbaccc
+=== 출력 예시.1 ===
+7
+=== 입력 예시.2 ===
+ababcdcdababcdcd
+=== 출력 예시.2 ===
+9
+*/
+
+import java.util.Scanner;
+
+public class Main {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    
+
+    sc.close();
+  }
+}
