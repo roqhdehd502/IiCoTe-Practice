bf1f2aa0f7a6439f54b9e2475a7df722cacb2317
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-02-07 20:49:28 +0900

    Update ex12-3.java

--- CHAPTER12/ex12-3.java
+++ CHAPTER12/ex12-3.java
@@ -3,33 +3,36 @@
 === 입력 예시.1 ===
 aabbaccc
 === 출력 예시.1 ===
-7
+2a2ba3c => 7
 === 입력 예시.2 ===
 ababcdcdababcdcd
 === 출력 예시.2 ===
-9
+2ab2cd2ab2cd => 9
+=== 입력 예시.3 ===
+abcabcdede
+=== 출력 예시.3 ===
+2abcdede => 8 (같은 문자열의 갯수로 자른다)
 */
 
 import java.util.Scanner;
-import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
     //Scanner sc = new Scanner(System.in);
     
-    String s = "aabbaccc";
+    String s = "ababcdcdababcdcd";
     // String s = sc.nextLine(); // 입력되는 문자열
     // sc.close();
-    char[] cArr = s.toCharArray(); // 문자열을 문자 배열로 변환
-    ArrayList<Object> cTemp = new ArrayList<>(); // 임시로 담을 문자
+    String sTemp1 = "";
+    String sTemp2 = "";
 
-    for(int i=1; i<cArr.length; i++) {
-      if(cArr[i-1] != cArr[i]) {
-        cTemp.add(cArr[i]);
+    for(int i=1; i<s.length(); i++) {
+      if(s.charAt(i-1) != s.charAt(i)) {
+        if(sTemp1.contains(sTemp2)) { // 
+          sTemp1 = "ababcdcd";
+          sTemp2 = "";
+        }
       }
     }
-    
-    System.out.println(cTemp);
-    System.out.println(cTemp.size());
   }
 }
