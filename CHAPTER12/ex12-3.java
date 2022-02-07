58d5b15e632953e84484516c0598acf92c7581f8
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-02-07 20:23:59 +0900

    Update ex12-3.java

--- CHAPTER12/ex12-3.java
+++ CHAPTER12/ex12-3.java
@@ -11,13 +11,25 @@ ababcdcdababcdcd
 */
 
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-
+    //Scanner sc = new Scanner(System.in);
     
+    String s = "aabbaccc";
+    // String s = sc.nextLine(); // 입력되는 문자열
+    // sc.close();
+    char[] cArr = s.toCharArray(); // 문자열을 문자 배열로 변환
+    ArrayList<Object> cTemp = new ArrayList<>(); // 임시로 담을 문자
 
-    sc.close();
+    for(int i=1; i<cArr.length; i++) {
+      if(cArr[i-1] != cArr[i]) {
+        cTemp.add(cArr[i]);
+      }
+    }
+    
+    System.out.println(cTemp);
+    System.out.println(cTemp.size());
   }
 }
