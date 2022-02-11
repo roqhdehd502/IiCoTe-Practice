087cf5ca63b250783d466888060f4567b8c7380c
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-02-11 20:58:11 +0900

    Update ex12-7.java

--- CHAPTER12/ex12-7.java
+++ CHAPTER12/ex12-7.java
@@ -36,7 +36,6 @@ class Main {
         if(arr[i][j] == 1) { // 집
           r1.add(j+1);
           c1.add(i+1);
-          //distance(r1, r2, c1, c2);
         } else if(arr[i][j] == 2) { // 치킨 집
           r2.add(j+1);
           c2.add(i+1);
@@ -46,10 +45,20 @@ class Main {
     sc.close();
 
     /* ToDo: 각 행과 열의 값 간의 거리를 계산 후 최솟값 산출하기*/
-    
+    int temp1 = 0;
+    int temp2 = 0;
+    ArrayList<Integer> cd = new ArrayList<>(); // 치킨 거리 
+    for(int i; i<c2.size(); i++) {
+      for(int j; j<c1.size(); j++) {
+        distance(r1.get(j), r2.get(i), c1.get(j), c2.get(i));
+      }
+      cd.add(Math.min(temp1, temp2)); // 해당 최솟값을 산출(치킨 거리)
+    }
 
+    int sum = 0;
+    for(int i : cd) { sum += cd; } // 도시의 치킨 거리
+    System.out.println(sum); // 결과값 출력
   }
-
   
   private static int distance(int r1, int r2, int c1, int c2) {
     return Math.abs(r1-r2) + Math.abs(c1-c2);
