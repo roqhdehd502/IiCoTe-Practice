2297b7b44bb44c2f2862f35c62d9b8d6f0e3c16b
Minwoo Na <47406388+roqhdehd502@users.noreply.github.com>
2022-03-22 21:22:54 +0900

    Update ex15-4.java

--- CHAPTER15/ex15-4.java
+++ CHAPTER15/ex15-4.java
@@ -1,19 +1,9 @@
 /**
 15-04. 가사 검색
 
-=== 입력예시 ===
-6 5
-frodo
-front
-frost
-frozen
-frame
-kakao
-fro??
-?????o
-fr???
-fro???
-pro?
+=== 입력 예시 ===
+"frodo", "front", "frost", "frozen", "frame", "kakao"
+"fro??", "????o", "fr???", "fro???", "pro?"
 
 === 출력 예시 ===
 3 2 4 1 0
@@ -23,20 +13,13 @@ import java.util.*;
 
 class Main {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    // 입력값
+    String[] words = {"frodo", "front", "frost", "frozen", "frame", "kakao"};
+    String[] queries = {"fro??", "????o", "fr???", "fro???", "pro?"};
 
-    int words_cnt = sc.nextInt();
-    int queries_cnt = sc.nextInt();
-    
-    String[] words = new String[words_cnt];
-    for(int i=0; i<words_cnt; i++) { words[i] = sc.nextLine(); }
-
-    String[] queries = new String[queries_cnt];
-    for(int i=0; i<queries_cnt; i++) { queries[i] = sc.nextLine(); }
-    sc.close();
-
+    // 문자열 비교를 위한 queries 배열의 ? 키워드를 제외한 각 문자 개수
     int cmprcnt = 0;
-    int[] cmprwrds = new int[queries_cnt];
+    int[] cmprwrds = new int[queries.length];
     for(int i=0; i<queries.length; i++) {
       for(int j=0; j<queries[i].length(); j++) {
         if(queries[i].charAt(j) != '?') { cmprcnt++; }
@@ -45,8 +28,9 @@ class Main {
       cmprcnt = 0;
     }
 
+    // 함수를 호출하여 문자열이 포함된 갯수 산출
     int cnt = 0;
-    int[] result = new int[queries_cnt];
+    int[] result = new int[queries.length];
     for(int i=0; i<queries.length; i++) {
       for(int j=0; j<words.length; j++) {
         if(words[j].length() == queries[i].length()) {
@@ -57,9 +41,11 @@ class Main {
       cnt = 0;
     }
 
+    // 결과값 출력
     for(int i=0; i<result.length; i++) { System.out.print(result[i] + " "); }
   }
 
+  // 문자열 비교 함수
   public static int containCnt(String word, String query, int cmprwrd) {
     int cnt = 0;
   
